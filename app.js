a7a397940bd8171b3bf0bf3ff2c2d63c84e20e6c
shailly <jain.shai@husky.neu.edu>
2020-06-16 01:26:19 -0400

    delete and clear from local storage

--- app.js
+++ app.js
@@ -123,9 +123,32 @@ function removeTask(e){
   if(e.target.parentElement.classList.contains('delete-item')){
     if(confirm('are you sure?')){
       e.target.parentElement.parentElement.remove();
+
+      // remove from LS
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
   }
 }
+
+// Remove from Ls
+function removeTaskFromLocalStorage(taskItem){
+  let tasks;
+  if(localStorage.getItem('tasks') === null){
+    tasks = [];
+    }else{
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task, index){
+      if(taskItem.textContent === task){
+        tasks.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('tasks',JSON.stringify(tasks));
+}
+
+
 
 //clear tasks
 function clearTasks(){
@@ -135,6 +158,14 @@ function clearTasks(){
   while(taskList.firstChild){
 taskList.removeChild(taskList.firstChild);
   }
+
+  // clear  from Ls
+  clearTasksFromLocalStorage();
+}
+
+// Clear Task From LS
+function clearTasksFromLocalStorage(){
+  localStorage.clear();
 }
 
 //Filter Tasks
